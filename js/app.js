d2e22be6e5a331fb1558820fa4b72761286db64c
lunaroyster <oneinaquadrillion@live.com>
2017-01-20 15:25:19 +0000

    Changed date to 19th

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ app.controller('homeController', function($scope) {
 var clock;
 $(document).ready(function() {
 	var currentDate = new Date();
-	var futureDate  = new Date('May 18 2017 13:00:00 GMT+0530');
+	var futureDate  = new Date('May 19 2017 13:00:00 GMT+0530');
 	var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
 	clock = $('.timer').FlipClock(diff, {
 		clockFace: 'DailyCounter',
